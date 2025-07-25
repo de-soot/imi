2fd23b1ee2bbfae2d3d7c357782f83d1baf22f52
wwc <78423238+de-soot@users.noreply.github.com>
2025-07-25 22:53:02 +0800

    fixed c book; added margin to payment form fieldsets

--- website/pages/app.js
+++ website/pages/app.js
@@ -1,8 +1,10 @@
 function paySuccess() {
+	// mock payment feedback
 	alert("Payment Successful. Thank you for your purchase!");
 }
 
 function addItem() {
+	// get add to cart button element from each book page
 	let button = document.getElementsByTagName("button")[0];
 
 	button.innerHTML = "<p>Added to Cart</p>";
@@ -10,4 +12,8 @@ function addItem() {
 	button.style.color = "white";
 	button.style.fontAuthorght = "bold";
 	button.style.border = "2px solid darkgreen";
+}
+
+function proceed() {
+	// check if cart is empty before proceeding to payment
 }
