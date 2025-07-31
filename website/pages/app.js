8043601f639e204447c28a0cb39e83945682078b
wwc <78423238+de-soot@users.noreply.github.com>
2025-07-31 17:03:13 +0800

    added localstorage functionality to enter name prompt

--- website/pages/app.js
+++ website/pages/app.js
@@ -1,5 +1,7 @@
 // remember to allow `script` for `file:///` in NoScript extension
 
+// show number of cart items in navbar next to CART
+
 function paySuccess() {
 	// mock payment feedback
 	alert("Payment Successful. Thank you for your purchase!");
@@ -9,12 +11,15 @@ function addItem() {
 	// get add to cart button element from each book page
 	let buyBox = document.getElementById("buy");
 	let button = buyBox.getElementsByTagName("button")[0];
+
 	// give feedback on button click
 	button.innerHTML = "<p>Added to Cart</p>";
 	button.style.backgroundColor = "green";
 	button.style.color = "white";
 	button.style.fontAuthorght = "bold";
 	button.style.border = "2px solid darkgreen";
+
+	// add book cover, title, price, and quantity (default: 1) to localstorage to display in cart
 }
 
 function proceed() {
