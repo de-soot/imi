5075b6fb36ed6c2694ff931395d3393dfd35b830
wwc <78423238+de-soot@users.noreply.github.com>
2025-08-02 12:02:09 +0800

    added mdn web docs entries to references and commented link border effect used in index.html from styles.css; other javascript comments

--- website/pages/app.js
+++ website/pages/app.js
@@ -2,8 +2,7 @@
 
 // show number of cart items in navbar next to CART
 
-function paySuccess() {
-	// mock payment feedback
+function paySuccess() { // mock payment feedback
 	alert("Payment Successful. Thank you for your purchase!");
 }
 
@@ -20,6 +19,18 @@ function addItem() {
 	button.style.border = "2px solid darkgreen";
 
 	// add book cover, title, price, and quantity (default: 1) to localstorage to display in cart
+	let imgLink = document.getElementsByTagName("img")[0].src;
+	let bookTitle = document.getElementsByTagName("h1")[0].innerText;
+	let price = buyBox.getElementsByTagName("p")[0].innerText.substring(7); // get substring of price $XX.XX
+
+	let items = [];
+
+	if(localStorage.getItem("items")) { // if items not null; load existing items into variable to
+		items = JSON.parse(localStorage.getItem("items"));
+	}
+
+	items.push({"bookTitle": bookTitle, "imgLink": imgLink, "price": price, "qty": 1});
+	localStorage.setItem("items", JSON.stringify(items));
 }
 
 function proceed() {
